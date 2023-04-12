ca6eee6a6c3ebd4fd491346e3b419b284396fe55
baranxe1 <130478041+baranxe1@users.noreply.github.com>
2023-04-12 11:39:48 +0300

    Update Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -52,8 +52,8 @@ const Home = () => {
           iletişim araçlarımızdır.
         </p>
            
-        <p>𓆉 Hicr Suresi 97. Ayet:<br />
-        Göğsünün daraldığını biliyoruz..<br /> (ama sabret, Seni zafere ulaştıracağız.)<br /> <br />
+        <p>❁ Hicr Suresi 97. Ayet :<br />
+        Göğsünün daraldığını biliyoruz...<br /> (ama sabret, Seni zafere ulaştıracağız.)<br /> <br />
         {' '}
           <Link className='font-bold' href='https://arjenxyz.vercel.app/mesaj.html'>
            🧡 Şifreli Hatıra Mesajı
